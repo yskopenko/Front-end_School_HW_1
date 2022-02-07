c4a98c3409410b27f8cb03075da6a138b3829f69
Yuliia Skopenko <julia.skopenko@gmail.com>
2022-02-08 00:59:32 +0200

    add hw4

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,29 @@ import './App.css';
 import Header from './components/header/Header';
 import PersonalFeed from './pages/personalFeed/PersonalFeed';
 import User from './pages/user/User';
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme, GlobalStyles } from "./styled/theme";
 
 function App() {
+
+  const [theme, setTheme] = useState("light");
+
+  const switchTheme = () => {
+      theme === "light" ? setTheme("dark") : setTheme("light");
+    };
   return (
+
+
     <div className="App">
       <Header />
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <GlobalStyles />
       <div className="container mt-3">
         <div className="row">
           <div className="col-sm-9">
             <div className="mb-9">
+            <button onClick={switchTheme}>Switch Theme</button>
               <Routes>
                 <Route path="/" element={<PersonalFeed />} />
                 <Route path="user/:username" element={<User />} />
@@ -21,6 +35,7 @@ function App() {
           </div>
         </div>
       </div>
+      </ThemeProvider>
     </div>
   );
 }
